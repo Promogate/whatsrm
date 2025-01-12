46d5d3d19d3d49522ecdb84272147f398581e4a4
Augusto <gutodiasdev@gmail.com>
2025-01-11 23:30:58 -0300

    refact(interface): add findById method in CustomerRepository

--- src/core/ports/repositories/CustomerRepository.ts
+++ src/core/ports/repositories/CustomerRepository.ts
@@ -3,4 +3,5 @@ import { Customer } from '@core/domain/Customer';
 export interface CustomerRepository {
   save(customer: Customer): Promise<void>;
   findByEmail(email: string): Promise<Customer | null>;
+  findById(customerId: string): Promise<Customer | null>;
 }
